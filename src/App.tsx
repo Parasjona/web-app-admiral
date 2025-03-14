7b6472e68ce20f6761fe9de8dfcaf19226230340
Larisa Markova <lovemirror@rambler.ru>
2025-03-14 12:58:42 +0300

    Add example for Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,43 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import "./App.css";
-import { lightThemeClassName } from "@admiral-ds/web";
-import { DatePicker } from "@admiral-ds/date-picker";
-import { maskitoDateOptionsGenerator } from "@maskito/kit";
-import { useMaskito } from "@maskito/react";
 
-import { T } from "@admiral-ds/react-ui";
-
-const dateOptions = maskitoDateOptionsGenerator({ mode: "dd/mm/yyyy" });
-
-const defaultInputProps = {
-  placeholder: "Введите дату",
-  dataPlaceholder: "дд.мм.гггг",
-  value: "11.",
-} satisfies React.ComponentProps<typeof DatePicker>["inputProps"];
+import { Carousel, T } from "@admiral-ds/react-ui";
+import { CarouselTestExample } from "./carouselTestExample";
 
 function App() {
-  const [inputValue, setInputValue] = useState(defaultInputProps.value);
-  const maskedDateInputRef = useMaskito({ options: dateOptions });
-
-  const dateProps = {
-    inputProps: {
-      ...defaultInputProps,
-      value: inputValue,
-      onInput: (e) => setInputValue(e.currentTarget.value),
-      ref: maskedDateInputRef,
-    },
-  } satisfies React.ComponentProps<typeof DatePicker>;
-
-  useEffect(() => {
-    document.body.classList.add(...lightThemeClassName.split(" "));
-  }, []);
+  const items = [
+    <img
+      key="1"
+      src="https://www.fonstola.ru/images/201304/fonstola.ru_92517.jpg"
+      alt="placeholder"
+    />,
+    <img
+      key="2"
+      src="https://masyamba.ru/картинки-котят/33-милый-котенок.jpg"
+      alt="placeholder"
+    />,
+    <img
+      key="3"
+      src="https://en.free-wallpapers.su/data/media/2329/big/dog1650.jpg"
+      alt="placeholder"
+    />,
+    <img
+      key="4"
+      src="https://furman.top/uploads/posts/2023-08/1691005855_furman-top-p-zastavka-na-rabochii-stol-kotiki-krasivo-36.jpg"
+      alt="placeholder"
+    />,
+    <img
+      key="5"
+      src="https://wp-s.ru/wallpapers/5/1/315816524181182/troe-malenkix-shhenkov-xaski.jpg"
+      alt="placeholder"
+    />,
+    <img
+      key="6"
+      src="https://www.fonstola.ru/images/201910/fonstola.ru_349629.jpg"
+      alt="placeholder"
+    />,
+  ];
 
   return (
     <div>
@@ -42,7 +48,7 @@ function App() {
         <T font="Subtitle/Subtitle 1" as="p">
           Пример компонента
         </T>
-        <DatePicker {...dateProps} />
+        <CarouselTestExample />
       </div>
     </div>
   );
---
+++ src/carouselTestExample.tsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+
+import { Carousel } from "@admiral-ds/react-ui";
+
+const CarouselContainer = styled.div`
+  width: 70%;
+  margin: auto;
+`;
+
+const items = [
+  <img
+    key="1"
+    src="https://www.fonstola.ru/images/201304/fonstola.ru_92517.jpg"
+    alt="placeholder"
+  />,
+  <img
+    key="2"
+    src="https://masyamba.ru/картинки-котят/33-милый-котенок.jpg"
+    alt="placeholder"
+  />,
+  <img
+    key="3"
+    src="https://en.free-wallpapers.su/data/media/2329/big/dog1650.jpg"
+    alt="placeholder"
+  />,
+  <img
+    key="4"
+    src="https://furman.top/uploads/posts/2023-08/1691005855_furman-top-p-zastavka-na-rabochii-stol-kotiki-krasivo-36.jpg"
+    alt="placeholder"
+  />,
+  <img
+    key="5"
+    src="https://wp-s.ru/wallpapers/5/1/315816524181182/troe-malenkix-shhenkov-xaski.jpg"
+    alt="placeholder"
+  />,
+  <img
+    key="6"
+    src="https://www.fonstola.ru/images/201910/fonstola.ru_349629.jpg"
+    alt="placeholder"
+  />,
+];
+
+export const CarouselTestExample = () => {
+  const [mouseInCarousel, setMouseInCarousel] = useState(false);
+  const handleMouseEnterCarousel = () => setMouseInCarousel(true);
+  const handleMouseLeaveCarousel = () => setMouseInCarousel(false);
+
+  const [current, setCurrent] = useState(0);
+  const handleCurrentItemChange = (newValue: number) => {
+    setCurrent(newValue);
+  };
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (!mouseInCarousel) {
+        setCurrent((prev) => (prev + 1) % items.length);
+      }
+    }, 3000);
+    return () => clearInterval(timer);
+  }, [setCurrent, mouseInCarousel]);
+
+  return (
+    <CarouselContainer>
+      <Carousel>{items}</Carousel>
+      {/* <Carousel
+        currentItem={current}
+        onCurrentItemChange={handleCurrentItemChange}
+        onMouseEnter={handleMouseEnterCarousel}
+        onMouseLeave={handleMouseLeaveCarousel}
+        infiniteScroll
+        showButtons
+      >
+        {items}
+      </Carousel> */}
+    </CarouselContainer>
+  );
+};
